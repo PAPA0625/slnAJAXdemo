6b037514bd3f555753d6a46c8183ee93b4bc9aa6
andrew lee <bluesky0308@gmail.com>
2023-11-14 18:15:37 +0800

    加入專案檔案。

---
+++ prjAJAXdemo/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAJAXdemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "小趴";
+            }
+            return Content($"<h2>{name}</h2>", "text/html", System.Text.Encoding.UTF8);
+
+        }
+    }
+}
---
+++ prjAJAXdemo/ViewModels/CHW1ViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace prjAJAXdemo.ViewModels
+{
+    public class CHW1ViewModel
+    {
+        public string hw標題 { get; set; }
+        public string hw內文 { get; set; }
+        public string hw照片 { get; set; }
+    }
+}
